79faa5e52da4046f33cc8c335767a00d352bbc1c
NikitaGamer64 <nikitakordonov@mail.ru>
2022-10-18 13:24:35 +0300

    Tried to solve the project run problem
    Still gives me a “directly launch” issue

---
+++ App.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epic_Tetris_Super_Mario_Maker_2
+{
+    public class App
+    {
+        public MainWindow mainWindow;
+        public App()
+        {
+            mainWindow = new MainWindow();
+        }
+        public void Start()
+        {
+            mainWindow.ShowDialog();
+        }
+    }
+}
