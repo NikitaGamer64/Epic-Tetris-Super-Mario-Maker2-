ac514e28ec430df1e8ad0979f6a3985b62e2e0f5
NikitaGamer64 <36325411+NikitaGamer64@users.noreply.github.com>
2022-10-18 12:21:37 +0300

    Adding files that are not in folders
    Wondering why they didn’t upload in a Github Desktop

---
+++ DesignSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epic_Tetris_Super_Mario_Maker_2
+{
+    public class DesignSettings
+    {
+    }
+}
