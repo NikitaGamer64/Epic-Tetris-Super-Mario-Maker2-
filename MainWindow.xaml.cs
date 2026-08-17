41adeea7bf5c1340b5869534f732766d46dca902
NikitaGamer64 <nikitakordonov@mail.ru>
2026-08-18 00:34:18 +0300

    To actually see the next block coming

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -98,10 +98,16 @@ namespace Epic_Tetris__Super_Mario_Maker_2_
             }
         }
 
+        private void DrawNextBlock(BlockQueue blockQueue)
+        {
+            Block next = blockQueue.NextBlock;
+            NextImage.Source = blockImages[next.Id];
+        }
         private void Draw(GameState gameState)
         {
             DrawGrid(gameState.GameGrid);
             DrawBlock(gameState.CurrentBlock);
+            DrawNextBlock(gameState.BlockQueue);
         }
 
         private async Task GameLoop()
