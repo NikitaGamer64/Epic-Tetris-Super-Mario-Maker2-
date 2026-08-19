e1d567c5d118d1eb8e829d951ca03442edee757a
NikitaGamer64 <nikitakordonov@mail.ru>
2026-08-20 00:21:34 +0300

    Re-creating the project from scratch (attempt 3)

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Epic_Tetris_Super_Mario_Maker2_
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
